c369a0ed60ecebf1d06a991984e8f0581eb6fc2b
Jaeho Jeong <ywm2004@gmail.com>
2024-04-13 16:45:21 +0900

    #18 - 비지니스 로직 구현
    화면에 필요한 비지니스 로직 구현

--- src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
+++ src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
@@ -29,9 +29,11 @@ import static org.mockito.BDDMockito.*;
 @ExtendWith(MockitoExtension.class)
 class ArticleServiceTest {
 
-    @InjectMocks private ArticleService sut;
+    @InjectMocks
+    private ArticleService sut;
 
-    @Mock private ArticleRepository articleRepository;
+    @Mock
+    private ArticleRepository articleRepository;
 
     @DisplayName("검색어 없이 게시글을 검색하면, 게시글 페이지를 반환한다.")
     @Test
@@ -55,14 +57,14 @@ class ArticleServiceTest {
         SearchType searchType = SearchType.TITLE;
         String searchKeyword = "title";
         Pageable pageable = Pageable.ofSize(20);
-        given(articleRepository.findByTitle(searchKeyword, pageable)).willReturn(Page.empty());
+        given(articleRepository.findByTitleContaining(searchKeyword, pageable)).willReturn(Page.empty());
 
         // When
         Page<ArticleDto> articles = sut.searchArticles(searchType, searchKeyword, pageable);
 
         // Then
         assertThat(articles).isEmpty();
-        then(articleRepository).should().findByTitle(searchKeyword, pageable);
+        then(articleRepository).should().findByTitleContaining(searchKeyword, pageable);
     }
 
     @DisplayName("게시글을 조회하면, 게시글을 반환한다.")
