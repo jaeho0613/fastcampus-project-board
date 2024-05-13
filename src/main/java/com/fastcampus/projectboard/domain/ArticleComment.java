03164d2f7a20c365256e46ee27fc511280638ad1
Jaeho Jeong <ywm2004@gmail.com>
2024-05-13 23:13:14 +0900

    #42 - 대댓글 도메인 설계
    대댓글 도메인 안에서 부모, 자식 관계를 설정하는 코드를 추가

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -21,22 +23,45 @@ public class ArticleComment extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
-    @Setter @ManyToOne(optional = false) @JoinColumn(name = "userId") private UserAccount userAccount; // 유저 정보 (ID)
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article; // 게시글 (ID)
+    @Setter
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "userId")
+    private UserAccount userAccount; // 유저 정보 (ID)
+    
+    @Setter
+    @Column(updatable = false)
+    private Long prentCommentId; // 부모 댓글 ID
+    
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "prentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
 
-    @Setter @Column(nullable = false, length = 500) private String content; // 본문
+    @Setter
+    @Column(nullable = false, length = 500)
+    private String content; // 본문
 
 
-    protected ArticleComment() {}
+    protected ArticleComment() {
+    }
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long prentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.prentCommentId = prentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+    
+    public void addChildComment(ArticleComment child) {
+        child.setPrentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
